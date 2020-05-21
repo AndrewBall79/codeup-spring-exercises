c7c124b21594d5e76831743d19e3436238bf07ba
Andrew Ball <andy.bob.ball@gmail.com>
2020-05-21 17:40:37 -0500

    Thursday

--- src/main/java/com/codeup/springblogapp/controller/HomeController.java
+++ src/main/java/com/codeup/springblogapp/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("")
     public String welcome() {
         return "home";
     }
